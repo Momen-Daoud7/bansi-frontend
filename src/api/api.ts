b0c58c51c34782658648662421e76d1c8fab2126
monzer <almonther@ecom.io>
2024-12-26 21:48:00 +0200

    fix uri

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://147.93.27.52:3001/api',
+  baseURL: 'https://bansi.momendaoud.com//api',
   headers: {
     'Content-Type': 'application/json'
   }
